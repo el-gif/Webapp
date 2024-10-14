f669248af83ea6c3e9fa117a749597b7f7f68ee5
Alexander Peters <alexander.peters.100@gmail.com>
2024-10-14 11:58:28 +0100

    input_slider below map, no legend

--- app2.py
+++ app2.py
@@ -1,9 +1,8 @@
 from ipyleaflet import Map, Heatmap, LegendControl
 from ipyleaflet.velocity import Velocity
 from ipywidgets import Layout
-from shiny.express import ui, input
-from shinywidgets import render_widget, output_widget
-from shiny import reactive
+from shinywidgets import *
+from shiny import App, ui, reactive
 import pickle
 import numpy as np
 import itertools
@@ -11,6 +10,7 @@ import numpy as np
 from scipy.interpolate import griddata
 import xarray as xr
 from branca.colormap import linear
+
 
 # Beispiel-Windgeschwindigkeitsspanne (min und max Windgeschwindigkeit in m/s)
 min_wind_speed = 0
@@ -85,7 +85,6 @@ u_selection_interpol = interpolation(u_selection)
 v_selection_interpol = interpolation(v_selection)
 
 total_selection_interpol = []
-factor = 2 # interpolation for colormap, should be > 1
 
 for i in range(len(u_selection_interpol)):
 
@@ -95,109 +94,91 @@ for i in range(len(u_selection_interpol)):
 def remove_all_layers(m):
     for i in range(1, len(m.layers)):
         m.remove_layer(m.layers[1])
-        
-ui.h2("An ipyleaflet Map", style="display:none;"),  # Entferne oder verstecke die Überschrift, wenn nicht notwendig
 
-# Verwende CSS, um die Karte auf volle Bildschirmgröße zu setzen
-ui.tags.style("""
-    html, body {
-        margin: 0;
-        padding: 0;
-        height: 100%;
-        width: 100%;
-        overflow: hidden;
-    }
-"""),
-
-# Slider für die Zeitschritte (auf der Karte überlagert)
-ui.tags.div(
-    ui.input_slider("time_step", "Time Step (hours)", min=initial_step, max=final_step, step=step_size, value=initial_step),
-    style="position: absolute; top: 20px; left: 20px; background-color: white; padding: 10px; z-index: 1000;"
+# Benutzeroberfläche der Shiny App, Reihenfolge der Elemente innerhalb des tags.div() wichtig!
+app_ui = ui.page_fluid(
+        ui.tags.div(
+        output_widget("map"),
+        style="position:relative;"
+    ),
+    ui.tags.div(
+        ui.h2("Wind forecast Europe"),
+        style="position:relative; "
+    ),
+    ui.tags.div(
+        ui.input_slider("time_step", "Time step (hours)", min=initial_step, max=final_step, step=step_size, value=initial_step),
+        style="position:relative; ;"
+    )
 )
 
-from ipyleaflet import LegendControl
+# Serverlogik für die Shiny App
+def server(input, output, session):
+    
+    # Render das Leaflet-Widget mit render_widget
+    @output
+    @render_widget  # server logic is embedded directly using reactive.effect and render_widget decorators, no ui necessary to display element for map
+    def map():
+        m = Map(center=[(lat_min + lat_max) / 2, (lon_min + lon_max) / 2],
+                zoom=3,
+                tiles=f'https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{{z}}/{{x}}/{{y}}?access_token=pk.eyJ1IjoiZWwtZ2lmIiwiYSI6ImNtMXQyYWdsYzAwMGUycXFzdmY2eDFnaWMifQ.yirQoMK5TCdmZZUFUNXxwA',
+                attr='Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="https://www.mapbox.com/">Mapbox</a>',
+                layout=Layout(width='100%', height='90vh')
+            )
+        return m
 
-def create_legend_dict(colormap, steps=10):
-    """Erstellt ein Legenden-Dictionary aus einer kontinuierlichen Colormap."""
-    step_values = np.linspace(min_wind_speed, max_wind_speed, steps)
-    legend_dict = {f"{round(v, 2)} m/s": colormap(v) for v in step_values}
-    return legend_dict
+    @reactive.effect
+    def add_layer():
+        m = map.widget
+        remove_all_layers(m)
 
-def add_continuous_legend(m, colormap):
-    # Erstelle die Legende als Dictionary mit diskreten Schritten
-    legend_dict = create_legend_dict(colormap, steps=10)
-    legend_control = LegendControl(legend=legend_dict, position="bottomright")
-    m.add_control(legend_control)
+        # Slider time step
+        time_step = input.time_step()
+        step_index = int((time_step - initial_step) / step_size)
 
+        # Erzeuge das kartesische Produkt, um alle Kombinationen von Breiten- und Längengraden zu erhalten
+        grid_points = list(itertools.product(lats_selection, lons_selection))
 
+        # Entpacke die Koordinaten in separate Listen
+        latitudes, longitudes = zip(*grid_points)
 
-@render_widget  # server logic is embedded directly using reactive.effect and render_widget decorators, no ui necessary to display element for map
-def map():
-    m = Map(center=[(lat_min + lat_max) / 2, (lon_min + lon_max) / 2],
-            zoom=3,
-            tiles=f'https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{{z}}/{{x}}/{{y}}?access_token=pk.eyJ1IjoiZWwtZ2lmIiwiYSI6ImNtMXQyYWdsYzAwMGUycXFzdmY2eDFnaWMifQ.yirQoMK5TCdmZZUFUNXxwA',
-            attr='Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="https://www.mapbox.com/">Mapbox</a>',
-            layout=Layout(width='100%', height='100vh')
+        # Wende die Colormap auf die Windgeschwindigkeitswerte an (Farben)
+        heatmap_data = []
+        for lat, lon, wind_speed in zip(latitudes, longitudes, total_selection_interpol[step_index].flatten()):
+            # Füge die Position und die Windgeschwindigkeit (Farbe) zur Heatmap hinzu
+            heatmap_data.append((lat, lon, wind_speed))  # Heatmap arbeitet mit (lat, lon, intensity)
+
+        # Heatmap erstellen
+        heatmap = Heatmap(locations=heatmap_data, radius=5, blur=5, max_zoom=10)
+
+        # Füge die Heatmap zur Karte hinzu
+        m.add_layer(heatmap)
+
+        display_options = {
+            'velocityType': 'European Wind',
+            'displayPosition': 'bottomleft',
+            'displayEmptyString': 'No wind data'
+        }
+
+        wind_layer = Velocity(
+            data={
+                'u': (['lat', 'lon'], u_selection_interpol[step_index]),
+                'v': (['lat', 'lon'], v_selection_interpol[step_index]),
+                'lat': lats_selection,
+                'lon': lons_selection,
+            },
+            zonal_speed='u',  # Zonal (east-west) wind component
+            meridional_speed='v',  # Meridional (north-south) wind component
+            latitude_dimension='lat',
+            longitude_dimension='lon',
+            velocity_scale=0.01,  # Scale of the wind arrows
+            max_velocity=20,  # Maximum velocity for scaling the arrows
+            display_options=display_options  # Use the display options we defined
         )
-    return m
 
 
-@reactive.effect
-def add_layer():
-    m = map.widget
-    remove_all_layers(m)
+        m.add_layer(wind_layer)  # Add wind velocity layer to the map
 
-    # Slider time step
-    time_step = input.time_step()
-    step_index = int((time_step - initial_step) / step_size)
-
-    # Erzeuge das kartesische Produkt, um alle Kombinationen von Breiten- und Längengraden zu erhalten
-    grid_points = list(itertools.product(lats_selection, lons_selection))
-
-    # Entpacke die Koordinaten in separate Listen
-    latitudes, longitudes = zip(*grid_points)
-
-    # Wende die Colormap auf die Windgeschwindigkeitswerte an (Farben)
-    heatmap_data = []
-    for lat, lon, wind_speed in zip(latitudes, longitudes, total_selection_interpol[step_index].flatten()):
-        # Füge die Position und die Windgeschwindigkeit (Farbe) zur Heatmap hinzu
-        heatmap_data.append((lat, lon, wind_speed))  # Heatmap arbeitet mit (lat, lon, intensity)
-
-    # Heatmap erstellen
-    heatmap = Heatmap(locations=heatmap_data, radius=5, blur=5, max_zoom=10)
-
-    # Füge die Heatmap zur Karte hinzu
-    m.add_layer(heatmap)
-
-    # Füge die kontinuierliche Legende hinzu
-    add_continuous_legend(m, colormap)
-
-    display_options = {
-        'velocityType': 'European Wind',
-        'displayPosition': 'bottomleft',
-        'displayEmptyString': 'No wind data'
-    }
-
-    wind_layer = Velocity(
-        data={
-            'u': (['lat', 'lon'], u_selection_interpol[step_index]),
-            'v': (['lat', 'lon'], v_selection_interpol[step_index]),
-            'lat': lats_selection,
-            'lon': lons_selection,
-        },
-        zonal_speed='u',  # Zonal (east-west) wind component
-        meridional_speed='v',  # Meridional (north-south) wind component
-        latitude_dimension='lat',
-        longitude_dimension='lon',
-        velocity_scale=0.01,  # Scale of the wind arrows
-        max_velocity=20,  # Maximum velocity for scaling the arrows
-        display_options=display_options  # Use the display options we defined
-    )
-
-
-    m.add_layer(wind_layer)  # Add wind velocity layer to the map
-
-
-
+# Shiny App erstellen und starten
+app = App(app_ui, server)
 
 # to run: shiny run app2.py
